8c3a4bf424e41777730a518fca65e87c5523c8e8
Jack Domleo <jackdomleo7@hotmail.com>
2018-11-04 00:01:06 +0000

    Applied quote carousel and other small changes.

--- website.js
+++ website.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
 			event.preventDefault();
 
 			$('html, body').animate({
-				scrollTop: $($.attr(this, 'href')).offset().top-100
+				scrollTop: $($.attr(this, 'href')).offset()
 			}, 500);
 		});
 	});
