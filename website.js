badeafb290199844b86c35683a0d0f397f639e63
Jack Domleo <jackdomleo7@hotmail.com>
2019-02-28 10:57:50 +0000

    Reduced anchor tag offset.

--- website.js
+++ website.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
 		$('a[href^="#"]').click(function (event) {
 			event.preventDefault();
 			$('html, body').animate({
-				scrollTop: $($.attr(this, 'href')).offset().top-100
+				scrollTop: $($.attr(this, 'href')).offset().top-80
 			}, 500);
 		});
 	});
