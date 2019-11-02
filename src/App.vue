f715c169ceeca2a01ae4972e316a28dd69cee89f
Jack Domleo <jackdomleo7@hotmail.com>
2019-11-02 11:38:10 +0000

    npm run lint

--- src/App.vue
+++ src/App.vue
@@ -19,12 +19,13 @@ export default class App extends Vue {}
 
 <style lang="scss">
 @import "./scss/main";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  text-align: center;
 }
 </style>
