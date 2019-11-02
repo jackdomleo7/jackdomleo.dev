b701965d9ba8c0ea37d65dbd6a11b9f39934f803
Jack Domleo <jackdomleo7@hotmail.com>
2019-11-02 11:30:54 +0000

    Added global styles

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "./scss/main";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
