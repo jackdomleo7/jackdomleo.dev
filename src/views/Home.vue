bd4381a3e56d6e2e278ef11426fffdaf53b9c10c
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2019-11-13 15:58:12 +0000

    Added portrait image with shine effect

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,17 @@
       <h1 class="intro__welcome">Hello there!<br>Let me introduce myself...<br>I'm Jack Domleo.</h1>
     </section>
     <section class="home__spotlight spotlight">
-      <figure style=" background-color: deeppink;
-  height: 100%;
-  margin: 0;
-  width: 100%;">Image</figure>
+      <figure class="spotlight__me">
+        <div class="spotlight__portrait">
+          <img src="/img/jack-domleo-portrait.jpg" alt="Jack Domleo portrait" />
+        </div>
+      </figure>
       <figure style=" background-color: deepskyblue;
-  height: 100%;
-  margin: 0;
-  width: 100%;">CSS Is Awesome</figure>
+  margin: 0;">CSS Is Awesome</figure>
       <figure style=" background-color: orangered;
-  height: 100%;
-  margin: 0;
-  width: 100%;">Mitrefinch logo</figure>
+  margin: 0;">Mitrefinch logo</figure>
       <figure style=" background-color: lawngreen;
-  height: 100%;
-  margin: 0;
-  width: 100%;">Something else</figure>
+  margin: 0;">Something else</figure>
     </section>
   </div>
 </template>
@@ -40,7 +35,7 @@
 
 .home {
   display: grid;
-  grid-template: auto / 1fr 1fr;
+  grid-template: auto / 1.1fr 0.9fr;
   grid-template-areas: "intro spotlight";
 
   @media (max-width: 1290px) {
@@ -98,6 +93,45 @@
 
   @media (max-width: 480px) {
     grid-template: 1fr / 1fr;
+  }
+
+  &__me {
+    background-color: fade_out($grey-900, 0.5);
+    margin: 0;
+    padding: 1rem;
+  }
+
+  &__portrait {
+    border-radius: 0.375rem;
+    overflow: hidden;
+    position: relative;
+
+    &::before {
+      background: linear-gradient(to right, fade_out($grey-50, 1) 0%, fade_out($grey-50, 0.7) 100%);
+      content: "";
+      display: block;
+      height: 100%;
+      left: -75%;
+      position: absolute;
+      top: 0;
+      transform: skewX(-25deg);
+      width: 50%;
+      z-index: 2;
+    }
+
+    &:hover::before {
+      animation: shine 0.85s;
+    }
+
+    @keyframes shine {
+      100% {
+        left: 125%;
+      }
+    }
+
+    img {
+      display: block;
+    }
   }
 }
 </style>
