336bf1336112aad4708dff2be7800a3163c3c6bd
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2019-11-19 00:55:08 +0000

    Added Who Am I and What Is My Developer Experience questions in about section

---
+++ src/partials/about/WhoAmI.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>I'm an enthusiastic web developer, based in the East Midlands, UK with a passion for front-end development.</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class WhoAmI extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,75 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <section class="about__title">
+      <h1>About me</h1>
+    </section>
+    <article v-for="(question, index) in questions" :key="index" class="about__article">
+      <h2 class="article__title">{{ question.title }}</h2>
+      <component class="article__content" :is="question.component"></component>
+    </article>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import WhoAmI from '@/partials/about/WhoAmI.vue';
+import WhatIsMyDeveloperExperience from '@/partials/about/WhatIsMyDeveloperExperience.vue';
+
+@Component({
+  components: { WhoAmI, WhatIsMyDeveloperExperience },
+})
+export default class About extends Vue {
+  private readonly questions: object[] = [
+    {
+      title: 'Who am I?',
+      component: WhoAmI,
+    },
+    {
+      title: 'What is my developer experience?',
+      component: WhatIsMyDeveloperExperience,
+    },
+    {
+      title: 'What about development interests me the most?',
+      component: WhoAmI,
+    },
+    {
+      title: 'What do I do in my spare time?',
+      component: WhoAmI,
+    },
+  ];
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/colours";
+
+.about {
+  &__title {
+    h1 {
+      font-size: 2.8rem;
+      margin: 0;
+    }
+  }
+
+  &__article {
+    margin: 4rem 0;
+
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+  }
+}
+
+.article {
+  &__title {
+    font-size: 1.3rem;
+    font-style: italic;
+    margin-bottom: 1.4rem;
+  }
+
+  &__content {
+    color: $grey-75;
+    font-size: 1rem;
+  }
+}
+</style>
