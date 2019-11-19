2e3085b63070d2efa5e8f11a0d02c349175d0238
Jack Domleo <jackdomleo7@hotmail.com>
2019-11-19 00:55:08 +0000

    Added Who Am I and What Is My Developer Experience questions in about section

---
+++ src/partials/about/WhatIsMyDeveloperExperience.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>I started my career as a developer in <time datetime="2018-09">September 2018</time> at <a class="link--paragraph" href="https://en.wikipedia.org/wiki/Flybmi" rel="noreferrer" target="_blank">flybmi</a> as an IT Developer Apprentice. Here I was introduced to many new web technologies and standards where my skills would begin and grow.</p>
+    <p>When flybmi filed for administration in <time datetime="2019-02">February 2019</time>, I started a new role as a Junior Developer at <a class="link--paragraph" href="https://www.mitrefinch.co.uk/" target="_blank" rel="norefferer">Mitrefinch</a> in <time datetime="2019-03">March 2019</time> and was promoted to a Developer in <time datetime="2019-05">May 2019</time>.</p>
+    <p>My current role is a Developer at <a class="link--paragraph" href="https://www.mitrefinch.co.uk/" target="_blank" ref="norefferer">Mitrefinch</a>, where I am working on cool, modern software with interesting programming languages, methodologies and frameworks. I am offered many benefits such as <a class="link--paragraph" href="https://www.perkbox.com" rel="noreferrer" target="_blank">Perkbox</a>, flexible working, fresh fruit, health vouchers and one afternoon every fortnight for <abbr aria-label="Career Professional Development" data-balloon-pos="down">CPD</abbr>, where I can work on my personal development.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class WhatIsMyDeveloperExperience extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
