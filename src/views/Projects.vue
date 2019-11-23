23225dc52972fcd0dbf091a6f3601450e671c131
Jack Domleo <jackdomleo7@hotmail.com>
2019-11-23 13:56:08 +0000

    Initial projects page

---
+++ src/views/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="page--title">Projects</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Projects extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
