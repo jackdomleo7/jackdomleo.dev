ced9df69d1e65362b6c5052f1654bb02e59400ea
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2019-11-23 13:56:08 +0000

    Initial projects page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import Projects from '@/views/Projects.vue';
 import PageNotFound from '@/views/PageNotFound.vue';
 
 Vue.use(VueRouter);
@@ -16,6 +17,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About,
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: Projects,
   },
   {
     path: '/404',
