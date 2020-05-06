b5c3bb532856c30951b9188a38552801b143fae1
Jack Domleo <jackdomleo7@hotmail.com>
2020-05-06 01:31:56 +0100

    Created favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,9 +9,20 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'msapplication-TileColor', content: '#2d89ef' },
+      { name: 'msapplication-TileImage', content: '/favicons/mstile-144x144.png' },
+      { name: 'msapplication-config', content: '/browserconfig.xml' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
     link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '194x194', href: '/favicons/favicon-194x194.png' },
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicons/android-chrome-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/favicons/safari-pinned-tab.svg', color: '#5bbad5' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
