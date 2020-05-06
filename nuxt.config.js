f39fd6e9250b3eab96f7dde1827a686de8911f16
Jack Domleo <jackdomleo7@hotmail.com>
2020-05-06 01:39:42 +0100

    Added other metadata

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,10 @@ export default {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'name', content: 'Jack Domleo' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
       { name: 'msapplication-TileColor', content: '#2d89ef' },
       { name: 'msapplication-TileImage', content: '/favicons/mstile-144x144.png' },
