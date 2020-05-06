efa503f538f56bd4560fc7550b4305740f759a27
Jack Domleo <jackdomleo7@hotmail.com>
2020-05-06 16:11:55 +0100

    Added top gradient

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <div class="gradient"></div>
     <nuxt />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@keyframes Gradient {
+  0%{background-position:0% 50%}
+  50%{background-position:100% 50%}
+  100%{background-position:0% 50%}
+}
+
+.gradient {
+  animation: Gradient 10s ease infinite;
+  background: linear-gradient(50deg, var(--color-orange-deep) 0%, var(--color-orange-faint) 100%);
+  background-size: 400%;
+  width: 100vw;
+  height: 10px;
+  opacity: 0.8;
+}
+</style>
