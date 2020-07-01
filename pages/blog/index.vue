96202a20161e14315ed8f64cfa53cc8eb901014d
Jack Domleo <jackdomleo7@hotmail.com>
2020-07-01 23:02:31 +0100

    Added latest article to home page. Created ArticlesList component.

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -16,34 +16,7 @@
       </li>
     </ul>
     <textfield v-model="articleSearch" label="Filter articles" type="search" placeholder="Search..." @input="filterArticles" />
-    <ul v-if="articlesLoading" class="articles--skeleton" role="presentation">
-      <li v-for="i in 3" :key="i" class="article--skeleton" role="presentation">
-        <div class="skeleton__header" />
-        <div class="skeleton__meta" />
-        <div class="skeleton__description first" />
-        <div class="skeleton__description last" />
-      </li>
-    </ul>
-    <ul v-if="!articlesLoading" class="articles">
-      <li v-for="(blog, index) in filteredArticles" :key="index" :aria-setsize="filteredArticles.length" :aria-posinset="index + 1">
-        <article class="article">
-          <header>
-            <h2 class="article__title">
-              <nuxt-link :to="'/blog/' + blog.slug">
-                {{ blog.title }}
-              </nuxt-link>
-            </h2>
-            <ul class="article__meta">
-              <li><time :datetime="articleDate(blog.date).datetime">{{ articleDate(blog.date).date }}</time></li>
-              <li><span>{{ blog.readingTime }} {{ blog.readingTime === 1 ? 'minute' : 'minutes' }} read</span></li>
-            </ul>
-          </header>
-          <section class="article__body">
-            <p>{{ blog.description }}</p>
-          </section>
-        </article>
-      </li>
-    </ul>
+    <articles-list :articles="filteredArticles" :loading="{ loading: articlesLoading, skeletonCount: 3 }" />
   </page-template>
 </template>
 
@@ -51,18 +24,18 @@
 import { Vue, Component } from 'nuxt-property-decorator';
 import { format } from 'date-fns';
 import { PageTemplate, Textfield } from '@/components';
+import { ArticlesList } from '@/components/blog';
 import Search from '@/middleware/fuzzySearch';
 
-interface IArticleDate {
-  datetime: string;
-  date: string;
-}
-
 @Component({
-  components: { PageTemplate, Textfield },
+  components: { ArticlesList, PageTemplate, Textfield },
   head () {
     return {
-      title: 'Blog'
+      title: 'Blog',
+      meta: [
+        { hid: 'description', name: 'description', content: 'A list of all the articles written by me!' },
+        { hid: 'og:description', name: 'og:description', content: 'A list of all the articles written by me!' }
+      ]
     };
   }
 })
@@ -79,14 +52,6 @@ export default class Index extends Vue {
     this.articlesLoading = false;
   }
 
-  private articleDate (date: Date): IArticleDate {
-    const newDate = Date.parse(String(date));
-    return {
-      date: format(newDate, 'do MMMM yyyy'),
-      datetime: format(newDate, 'yyyy-MM-dd')
-    };
-  }
-
   private filterArticles () {
     if (this.articleSearch === '') {
       this.filteredArticles = this.articles;
@@ -100,8 +65,8 @@ export default class Index extends Vue {
     return (
       Search.fuzzySearch(article.title, this.articleSearch) ||
       Search.fuzzySearch(article.description, this.articleSearch) ||
-      Search.fuzzySearch(this.articleDate(article.date).date, this.articleSearch) ||
-      Search.fuzzySearch(this.articleDate(article.date).datetime, this.articleSearch) ||
+      Search.fuzzySearch(format(Date.parse(String(article.date)), 'do MMMM yyyy'), this.articleSearch) ||
+      Search.fuzzySearch(format(Date.parse(String(article.date)), 'yyyy-MM-dd'), this.articleSearch) ||
       Search.fuzzySearch(article.readingTime, this.articleSearch) ||
       Search.fuzzySearch(`${article.readingTime} minutes`, this.articleSearch) ||
       Search.fuzzySearch(article.hashtags.join(' '), this.articleSearch)
@@ -109,17 +74,6 @@ export default class Index extends Vue {
   }
 }
 </script>
-
-<style lang="scss">
-:root {
-  --skeleton-light: #444;
-  --skeleton-dark: #3a3a3a;
-}
-.theme--light {
-  --skeleton-light: #eee;
-  --skeleton-dark: #ddd;
-}
-</style>
 
 <style lang="scss" scoped>
 @import '@/assets/styles/shared/box';
@@ -135,109 +89,6 @@ export default class Index extends Vue {
 
   @media (min-width: 30em) {
     grid-template-columns: 1fr 1fr;
-  }
-}
-
-.articles {
-  padding-left: 0;
-  list-style-type: none;
-
-  > li {
-    margin: 4rem 0;
-
-    &:first-of-type {
-      margin-top: 0;
-    }
-
-    &:last-of-type {
-      margin-bottom: 0;
-    }
-  }
-
-  &--skeleton {
-    list-style-type: none;
-    padding-left: 0;
-    overflow-x: hidden;
-
-    li {
-      margin: 4rem 0;
-
-      &:first-of-type {
-        margin-top: 1.25rem;
-      }
-
-      &:last-of-type {
-        margin-bottom: 0;
-      }
-
-      > div {
-        animation: skeleton 5s ease infinite;
-        background: linear-gradient(90deg, var(--skeleton-light) 0%, var(--skeleton-light) 46%, var(--skeleton-dark) 50%, var(--skeleton-light) 54%, var(--skeleton-light) 100%);
-        background-size: 250%;
-
-        &.skeleton {
-          &__header {
-            height: 2rem;
-            width: 45%;
-            margin-bottom: 1.25rem;
-          }
-
-          &__meta {
-            height: 1.2rem;
-            width: 30%;
-            margin-bottom: 1.5rem;
-          }
-
-          &__description {
-            height: 1.1rem;
-
-            &.first {
-              width: 80vw;
-            }
-
-            &.last {
-              width: 35%;
-              margin-top: 0.5rem;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  @keyframes skeleton {
-  0% {
-    background-position: 100% 50%;
-  }
-  100% {
-    background-position: 0% 50%;
-  }
-}
-
-  .article {
-    &__title {
-      a {
-        text-decoration: none;
-      }
-    }
-
-    &__meta {
-      padding-left: 0;
-      display: flex;
-      align-items: center;
-
-      li {
-        &:nth-child(1) {
-          list-style-type: none;
-          margin-right: 1.5rem;
-          font-weight: 700;
-        }
-      }
-    }
-
-    &__body {
-      margin-top: 1.5rem;
-    }
   }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <page-template page-title="Hey, I'm Jack Domleo">
     <intro />
     <hr>
+    <latest />
+    <hr>
     <currently-doing />
     <hr>
     <top-projects />
@@ -26,10 +28,10 @@
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator';
 import { PageTemplate } from '@/components';
-import { Contact, CurrentlyDoing, Intro, Toolbox, TopProjects, Work } from '@/components/home';
+import { Contact, CurrentlyDoing, Intro, Latest, Toolbox, TopProjects, Work } from '@/components/home';
 
 @Component({
-  components: { Contact, CurrentlyDoing, Intro, PageTemplate, Toolbox, TopProjects, Work },
+  components: { Contact, CurrentlyDoing, Intro, Latest, PageTemplate, Toolbox, TopProjects, Work },
   head () {
     return {
       title: 'Home'
