73f59da68e23b142ab7ad85a3d076446bb688261
Jack Domleo <jackdomleo7@hotmail.com>
2020-07-01 13:27:56 +0100

    Fix /feed url

--- nuxt.config.js
+++ nuxt.config.js
@@ -127,7 +127,7 @@ export default {
   },
   feed: [
     {
-      path: '/feed.xml', // The route to your feed.
+      path: '/feed.xml',
       async create (feed) {
         feed.options = {
           title: 'Jack Domleo - blog',
@@ -152,9 +152,9 @@ export default {
           link: 'https://jackdomleo.dev'
         });
       },
-      cacheTime: 1000 * 60 * 15, // How long should the feed be cached
-      type: 'rss2', // Can be: rss2, atom1, json1
-      data: [''] // Will be passed as 2nd argument to `create` function
+      cacheTime: 1000 * 60 * 15,
+      type: 'rss2',
+      data: ['']
     }
   ],
   sitemap: {
