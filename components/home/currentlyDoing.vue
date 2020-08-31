f72a263ea457dd21b5d4a86a5c682f6d6dad3260
Jack Domleo <jackdomleo7@gmail.com>
2020-08-31 23:33:41 +0100

    Removed furloughed from currently doing

--- components/home/currentlyDoing.vue
+++ components/home/currentlyDoing.vue
@@ -4,11 +4,11 @@
     <ul class="currently">
       <li>
         <div>
-          Writing "Level-Up Your Career Today" - an eBook available to preorder <a href="https://gum.co/NQpxh" rel="nofollow noopener" target="_blank">here</a>
+          📝 Writing "Level-Up Your Career Today" - an eBook available to preorder <a href="https://gum.co/NQpxh" rel="nofollow noopener" target="_blank">here</a>
         </div>
       </li>
       <li><div>📚 Reading <a href="https://randallkanna.com/the-standout-developer" rel="nofollow noopener" target="_blank">The Standout Developer</a> by <a href="https://twitter.com/RandallKanna" rel="nofollow noopener" target="_blank">Randall Kanna</a></div></li>
-      <li><div>Part-time furloughed 😷</div></li>
+      <li><div>Planning my next big project - I'm thinking my own design system 🚀</div></li>
       <li>
         <div>
           Looking after my health ❤
@@ -42,6 +42,7 @@ export default class CurrentlyDoing extends Vue {}
     display: flex;
     align-items: center;
     justify-content: center;
+    text-align: center;
     padding: 1rem;
     border-radius: 0.75rem;
     background-color: rgba(0, 0, 0, 0.12);
