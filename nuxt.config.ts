ebbdb0f8e0916c7f618a984c03467da12a429eb7
Jack Domleo <jackdomleo7@gmail.com>
2021-08-17 21:58:21 +0100

    Add color-scheme metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'color-scheme', name: 'color-scheme', content: 'light dark' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
