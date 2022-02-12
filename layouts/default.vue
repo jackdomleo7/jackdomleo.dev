ea4d3d01afb3f7c9468cad182aec6c337d7fb0b0
Jack Domleo <jackdomleo7@gmail.com>
2022-02-12 13:48:26 +0000

    Add redirects for old blog articles

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,10 +18,29 @@ export default Vue.extend({
   components: { Navigation, SiteFooter },
   data () {
     return {
-      isMobile: false
+      isMobile: false,
+      blogRedirects: [ // Old blog articles before I put the year in the URL
+        '/i-wrote-an-ebook-here-is-what-i-learned',
+        '/why-the-term-t-shaped-is-better-than-the-term-full-stack',
+        '/how-i-optimised-my-online-presence-to-get-opportunities',
+        '/an-apprenticeship-interview',
+        '/focused-creativity-dont-lose-focus',
+        '/hover-css-media-query',
+        '/a-st-st-stuttering-developer',
+        '/how-i-went-from-a-retail-assistant-at-17-to-landing-a-developer-role-at-19',
+        '/why-to-not-support-internet-explorer',
+        '/learning-neumorphic-design',
+        '/all-day-hey-2020',
+        '/to-blog-or-not-to-blog'
+      ]
     }
   },
-  mounted (): void {
+  created () {
+    this.blogRedirects.forEach(redirect => {
+      if (`/blog${redirect}` === this.$route.path) this.$router.push(`/blog/2020${redirect}`)
+    })
+  },
+  mounted () {
     this.setResponsiveness()
     window.addEventListener('resize', this.setResponsiveness)
   },
