57b2c54fb98a6aeab3144d0f0460f45361735bf0
Jack Domleo <jackdomleo7@gmail.com>
2022-05-07 16:32:53 +0100

    Provided typings for custom environment variables

---
+++ process.env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    BASE_URL: string;
+    CONTACT_EMAIL: string;
+    GOOGLE_ANALYTICS_ID: string;
+    PRISMIC_ENDPOINT: string;
+    PRISMIC_ACCESS_TOKEN: string;
+  }
+}
