bcd2f21c0ea68ffa61f7ffeb9c4f8424b010aa05
Jack Domleo <jackdomleo7@gmail.com>
2022-06-02 17:29:21 +0100

    Product page header

--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -1,13 +1,38 @@
 <template>
-  
+  <div class="product">
+    <div class="product__inner">
+      <header class="header">
+        <div class="header__product">
+          <nuxt-img :src="productPage.data.images[0].image.url" :alt="productPage.data.images[0].image.alt" width="472" height="669" provider="prismic" />
+          <a v-if="productPage.data.goodreads_url.url" :href="productPage.data.goodreads_url.url" target="_blank" title="Add to Goodreads" class="header__goodreads">
+            <nuxt-img src="/images/goodreads-button.png" alt="Add to Goodreads" width="130" height="41" />
+            <span class="sr-only">Add to Goodreads</span>
+          </a>
+        </div>
+        <div class="header__details">
+          <h1 class="header__title">{{ productPage.data.title }}</h1>
+          <prismic-rich-text :field="productPage.data.short_description" class="header__description" />
+          <div class="header__ctas">
+            <btn :href="productPage.data.cta_url.url">{{ productPage.data.cta_text }}</btn>
+            <a v-if="productPage.data.amazon_url.url" :href="productPage.data.amazon_url.url" target="_blank" title="Buy on Amazon" class="header__amazon">
+              <nuxt-img src="/images/amazon-button.png" alt="Buy on Amazon" width="160" height="47" />
+              <span class="sr-only">Buy on Amazon</span>
+            </a>
+          </div>
+        </div>
+      </header>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { IPage, IPageProduct } from '@/types/cms'
+import Btn from '@/components/Btn.vue'
 
 export default Vue.extend({
   name: 'ProductSlug',
+  components: { Btn },
   async asyncData ({ $prismic, error, payload, params }) {
     const productPage: IPage<IPageProduct, 'product'> = payload || await $prismic.api.getByUID('product', params.slug)
     if (productPage) {
@@ -83,5 +108,99 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+.product {
+  margin-top: 1rem;
+  padding-inline: 1rem;
 
+  @media (min-width: $responsive-large-tablet) {
+    margin-top: 6rem;
+  }
+
+  &__inner {
+    max-width: 81rem;
+    margin-inline: auto;
+  }
+
+  .header {
+    display: grid;
+    gap: 2rem;
+    grid-template-columns: 1fr;
+
+    @media (min-width: $responsive-large-tablet) {
+      grid-template-columns: repeat(5, 1fr);
+    }
+
+    &__product {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+      place-self: center;
+
+      @media (min-width: $responsive-large-tablet) {
+        grid-column: span 2;
+      }
+    }
+
+    &__details {
+      align-self: center;
+      display: flex;
+      flex-direction: column;
+      padding-inline: 1rem;
+
+      @media (min-width: $responsive-large-tablet) {
+        grid-column: span 3;
+        padding-inline: 0;
+      }
+    }
+
+    &__title {
+      font-size: var(--text-title);
+      margin-top: 0;
+      margin-bottom: 1rem;
+
+      @media (min-width: $responsive-large-tablet) {
+        margin-bottom: 1.75rem;
+      }
+    }
+
+    &__description {
+      font-size: var(--text-large);
+      margin-top: 0;
+      margin-bottom: 2rem;
+
+      @media (min-width: $responsive-large-tablet) {
+        margin-bottom: 4rem;
+      }
+    }
+
+    &__ctas {
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      align-items: center;
+
+      @media (min-width: $responsive-large-tablet) {
+        align-self: flex-start;
+        gap: 1.5rem;
+      }
+    }
+
+    &__amazon {
+      display: flex;
+    }
+
+    &__goodreads {
+      display: flex;
+
+      @media (hover: hover) {
+        transition: filter ease var(--quick-animation-timing);
+
+        &:hover {
+          filter: brightness(90%);
+        }
+      }
+    }
+  }
+}
 </style>
