d4aa852de661596aa481cbe083303a07e2091543
Jack Domleo <jackdomleo7@gmail.com>
2022-10-23 20:48:08 +0100

    Set up SCSS & styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,19 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
     strict: true
+  },
+  css: [
+    'modern-normalize/modern-normalize.css',
+    '~/assets/styles/main.scss'
+  ],
+  vite: {
+    css: {
+      devSourcemap: true,
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/scss/variables/responsive.scss" as *;'
+        }
+      }
+    }
   }
 })
