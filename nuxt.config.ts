6333deb9489f0ec65ec262c01976366990b7133b
Jack Domleo <jackdomleo7@gmail.com>
2022-11-28 20:48:16 +0000

    Upgrade Nuxt to Nuxt 3 from a release candidate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  target: 'static',
+  ssr: false,
   typescript: {
     typeCheck: true,
     strict: true
