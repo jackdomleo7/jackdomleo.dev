56c0f530db556c0946dbe210900579c3ba6002d5
Jack Domleo <jackdomleo7@gmail.com>
2022-11-28 20:53:53 +0000

    Install @nuxt/image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,23 @@ export default defineNuxtConfig({
     'modern-normalize/modern-normalize.css',
     '~/assets/styles/main.scss'
   ],
+  modules: [
+    '@nuxt/image-edge'
+  ],
+  image: {
+    screens: {
+      smallmobile: 340,
+      standardmobile: 390,
+      largemobile: 450,
+      smalltablet: 640,
+      standardtablet: 768,
+      largetablet: 1000,
+      smalldesktop: 1366,
+      standarddesktop: 1920,
+      largedesktop: 2880,
+      '4kdesktop': 3840
+    }
+  },
   vite: {
     css: {
       devSourcemap: true,
