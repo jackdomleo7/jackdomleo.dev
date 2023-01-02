7357db34a1414a7340e1f506fbf679072901ed91
jackdomleo7 <jackdomleo7@users.noreply.github.com>
2023-01-02 00:58:42 +0000

    Deploying to gh-pages from @ jackdomleo7/jackdomleo.dev@d2ad18053ee2a0f8247b586a6092681adb77a259 🚀

--- _nuxt/static/1672535248/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1672535248",layout:"default",error:a,serverRendered:b,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9becacee"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
