2dc92b20a1e9444ef55e8a0a91031cadc299e05d
Jack Domleo <jackdomleo7@gmail.com>
2023-01-21 22:13:53 +0000

    Make site a static site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   typescript: {
     typeCheck: process.env.NODE_ENV === 'dev',
     strict: true
--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,11 +1,15 @@
-import * as contentful from 'contentful'
+import contentful from 'contentful'
+import { createClient } from 'contentful'
+
+// https://github.com/contentful/contentful.js/issues/1233
+const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
 
 export default defineNuxtPlugin(nuxtApp => {
   const config = useRuntimeConfig()
 
   return {
     provide: {
-      contentful: contentful.createClient({
+      contentful: createClientFunc({
         space: config.public.CTF_SPACE_ID,
         accessToken: config.public.CTF_CDA_ACCESS_TOKEN
       })
