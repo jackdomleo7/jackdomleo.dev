1b231739253062e8dffe1616e1ed1efeae8365de
Jack Domleo <jackdomleo7@gmail.com>
2023-02-05 14:34:06 +0000

    Use asyncData key to ensure a rerender

--- pages/blog/[year]/[slug].vue
+++ pages/blog/[year]/[slug].vue
@@ -51,7 +51,7 @@ const $img = useImage()
 const $route = useRoute()
 const config = useRuntimeConfig()
 
-const articleEntries = await useAsyncData((ctx) => { return ctx!.$contentful.getEntries<ContentfulEntries.Article>({ content_type: 'article', limit: 1, 'fields.slug': $route.params.slug })})
+const articleEntries = await useAsyncData(`article-${$route.params.slug}`, (ctx) => { return ctx!.$contentful.getEntries<ContentfulEntries.Article>({ content_type: 'article', limit: 1, 'fields.slug': $route.params.slug })})
 const article = formatCMSVariables(articleEntries.data.value!.items[0])
 
 const blogDetailsEntries = await useAsyncData((ctx) => { return ctx!.$contentful.getEntries<Pick<ContentfulEntries.BlogDetails, 'articleDisclaimer'>>({ content_type: 'blogDetails', limit: 1, select: ['fields.articleDisclaimer'] })})
