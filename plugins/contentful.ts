d05cb56f1fc4610992637fdca18c275798e1fa58
Jack Domleo <jackdomleo7@gmail.com>
2024-12-14 17:13:17 +0000

    Update dependencies

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,14 +1,11 @@
-import contentful, { createClient } from 'contentful' // eslint-disable-line import/default
-
-// https://github.com/contentful/contentful.js/issues/1233
-const createClientFunc = process.env.NODE_ENV === 'development' ? createClient : contentful.createClient
+import { createClient } from 'contentful'
 
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig()
 
   return {
     provide: {
-      contentful: createClientFunc({
+      contentful: createClient({
         space: config.public.CTF_SPACE_ID,
         accessToken: process.env.NODE_ENV === 'development' ? config.public.CTF_CDA_ACCESS_TOKEN_PREVIEW : config.public.CTF_CDA_ACCESS_TOKEN,
         host: process.env.NODE_ENV === 'development' ? 'preview.contentful.com' : undefined
