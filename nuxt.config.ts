4ea091db4f1474a591a2d235c9db8eabb67dab41
Jack Domleo <jackdomleo7@gmail.com>
2026-01-24 12:15:06 +0000

    perf: Remove unused under-the-hood Vue code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -107,6 +107,11 @@ export default defineNuxtConfig({
     ]
   },
   vite: {
+    define: {
+      __VUE_OPTIONS_API__: false,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
+    },
     css: {
       devSourcemap: true,
       preprocessorOptions: {
